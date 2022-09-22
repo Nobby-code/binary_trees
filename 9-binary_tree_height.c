16ab3cdc0532733d4b849237e8bc7bfb8b345b1c
Nobby-code <norbertguda0@gmail.com>
2022-09-22 12:16:55 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,32 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - function to find the height of a tree
+ * binary_tree_height - find the height of a tree
+ * @tree: tree to find the height of
  *
- * @tree: tree to find its height
- *
- * Return: height
+ * Return: Height from end point
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	int maxl;
+	int maxr;
+
+	/*Error checking if they pass us a NULL tree*/
 	if (tree == NULL)
 		return (0);
 
-	int left_len, right_len;
-
-	if ((tree->right == NULL) && (tree->left == NULL))
+	/*Actual base case to check against*/
+	if (tree->right == NULL && tree->left == NULL)
 		return (0);
 
-	left_len = binary_tree_height(tree->left);
-	right_len = binary_tree_height(tree->right);
-
-	if (left_len > right_len)
-	{
-		return (left_len + 1);
-	}
+	maxl = binary_tree_height(tree->left);
+	maxr = binary_tree_height(tree->right);
+	if (maxl > maxr)
+		return (maxl + 1);
 	else
-	{
-		return (right_len + 1);
-	}
+		return (maxr + 1);
 }
