bfb809ac3cd028d77fc0fe87dd9b4c852c4f9302
Nobby-code <norbertguda0@gmail.com>
2022-09-22 19:01:55 -0700

    14-binary_tree_balance.c fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,10 +10,11 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	int left_len;
+	int right_len;
+
 	if (tree == NULL)
 		return (0);
-
-	size_t left_len, right_len;
 
 	if (tree->right == NULL && tree->left == NULL)
 		return (0);
